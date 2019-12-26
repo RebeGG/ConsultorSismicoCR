587014680923aeb4cecc912cbfe1d2e094c1271b
RebeGG <49869273+RebeGG@users.noreply.github.com>
2019-12-26 15:57:12 -0600

    Add files via upload

---
+++ ConsultorSismos/src/consultorsismos/ConsultorSismo.java
@@ -0,0 +1,11 @@
+package consultorsismos;
+
+public class ConsultorSismo {
+
+    public static void main(String[] args) {
+        new ConsultorSismo().init();
+    }
+    
+    public void init(){}
+    
+}
