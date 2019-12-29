114115a7dccdaa891134f472038a0a3a01f37e34
RebeGG <49869273+RebeGG@users.noreply.github.com>
2019-12-29 17:03:00 -0600

    map.xml

--- ConsultorSismico/src/consultorsismico/Modelo/Model.java
+++ ConsultorSismico/src/consultorsismico/Modelo/Model.java
@@ -2,4 +2,16 @@ package consultorsismico.Modelo;
 
 public class Model {
     private Coordenada coordena;
+    
+    //Esta parte debe ir en el Modelo, creo
+    //Es para cargar la imagen y las coordenadas principales, pero esto solo debe hacerse una vez en toda la corrida del programa
+//    try {
+//            JAXBContext ctx = JAXBContext.newInstance(BaseMapa.class);
+//            Unmarshaller mrs = ctx.createUnmarshaller();
+//            BaseMapa base = (BaseMapa) mrs.unmarshal(new File("./map.xml"));
+//            System.out.println(base); ////esto solo prueba que esté funcionando todo, se quita despues
+//        } catch (JAXBException ex) {
+//            System.err.printf("Excepción: '%s'%n", ex.getMessage());
+//        }
+    
 }
