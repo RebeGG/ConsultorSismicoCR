02baced736b5cbc2f6b9fbdd6f3c088f6a709dae
fernandagonzalezarias <ferga0017@gmail.com>
2019-12-30 21:24:32 -0600

    Avances

--- ConsultorSismico/src/consultorsismico/ConsultorSismico.java
+++ ConsultorSismico/src/consultorsismico/ConsultorSismico.java
@@ -1,13 +1,62 @@
 package consultorsismico;
 
 import consultorsismico.Controlador.Controlador;
+import consultorsismico.Modelo.Conversion;
+import consultorsismico.Modelo.Coordenada;
+import consultorsismico.Modelo.Latitud;
+import consultorsismico.Modelo.Longitud;
+import consultorsismico.Modelo.PosicionImagen;
+import consultorsismico.Modelo.PosicionMapa;
 import consultorsismico.Vista.VentanaPrincipal;
 import javax.swing.SwingUtilities;
 
 public class ConsultorSismico {
 
     public static void main(String[] args) {
-        new ConsultorSismico().init();
+        //new ConsultorSismico().init();
+        
+        PosicionImagen primera = new PosicionImagen(86,67);
+        PosicionImagen segunda = new PosicionImagen(663,636);
+        Latitud lat = new Latitud(11,0,0,"N");
+        Longitud lon = new Longitud (86,0,0, "W");
+        Latitud lat2 = new Latitud(8,0,0,"N");
+        Longitud lon2 = new Longitud (83,0,0, "W");
+        PosicionMapa m1 = new PosicionMapa(lat, lon);
+        PosicionMapa m2 = new PosicionMapa(lat2, lon2);
+        Coordenada c1 = new Coordenada ("Primera", primera, m1);
+        Coordenada c2 = new Coordenada ("Segunda", segunda, m2);
+        Conversion a = new Conversion(c1, c2);
+        System.out.printf("Primera coordenada: %s\n", c1.toString());
+        System.out.printf("Primera latitud: %f\n", lat.latitudToDecimal());
+        System.out.printf("Primera longitud: %f\n", lon.longitudToDecimal());
+        System.out.printf("Segunda coordenada: %s\n", c2.toString());
+        System.out.printf("Segunda latitud: %f\n", lat2.latitudToDecimal());
+        System.out.printf("Segunda longitud: %f\n", lon2.longitudToDecimal());
+        
+        Latitud lat3 = new Latitud(9,0,0,"N");
+        Longitud lon3 = new Longitud (84,15,0, "W");
+        PosicionMapa m3 = new PosicionMapa(lat3, lon3);
+        
+        double aaaa= 84 + (15/60) + (0/3600);
+        float b= 1/2;
+        System.out.println(b);
+//        double x= a.longitudPix(lon3);
+//        double y = a.latitudPix(lat3);
+//        Double nwX= new Double(x);
+//        int nw = nwX.intValue();
+//        Double nyX= new Double(y);
+//        int ny = nyX.intValue();
+//        PosicionImagen tres = new PosicionImagen(nw,ny);
+//        Coordenada c3 = new Coordenada ("Tercera", tres, m3);
+//        
+//        System.out.printf("Tercera coordenada: %s\n", c3.toString());
+//        System.out.printf("Tercera latitud: %s\n", lat3.latitudToDecimal());
+//        System.out.printf("Ancho Pixeles: %d\n", a.anchoPixeles());
+//        System.out.printf("Altura Pixeles: %d\n", a.alturaPixeles());
+//        System.out.printf("Ancho Grados: %d\n", a.anchoGrados());
+//        System.out.printf("Altura Gradods: %d\n", a.alturaGrados());
+//        System.out.printf("Latitud t: %f\n", a.latitudT(lat3));
+//        System.out.printf("Longitud T: %f\n", a.longitudT(lon3));
     }
     
     public void init(){
--- ConsultorSismico/src/consultorsismico/Modelo/Conversion.java
+++ ConsultorSismico/src/consultorsismico/Modelo/Conversion.java
@@ -4,8 +4,28 @@ public class Conversion {
 
     Coordenada primera;
     Coordenada segunda;
-    Tamano tam;
+    //Tamano tam;
 
+    public Conversion(Coordenada primera, Coordenada segunda) {
+        this.primera = primera;
+        this.segunda = segunda;
+    }
+
+    public Coordenada getPrimera() {
+        return primera;
+    }
+
+    public void setPrimera(Coordenada primera) {
+        this.primera = primera;
+    }
+
+    public Coordenada getSegunda() {
+        return segunda;
+    }
+
+    public void setSegunda(Coordenada segunda) {
+        this.segunda = segunda;
+    }
     //Falta el constructor, setters y getters
     public int anchoPixeles() {
         return segunda.getPosI().getX() - primera.getPosI().getX();
@@ -13,15 +33,15 @@ public class Conversion {
 
     public int anchoGrados() {
         if (segunda.getPosM().getLongitud().getDireccion() == "W" && primera.getPosM().getLongitud().getDireccion() == "W") {
-            return -segunda.getPosM().getLongitud().getGrados() - (-segunda.getPosM().getLongitud().getGrados());
+            return -segunda.getPosM().getLongitud().getGrados() - (-primera.getPosM().getLongitud().getGrados());
         }
         if (segunda.getPosM().getLongitud().getDireccion() == "W" && primera.getPosM().getLongitud().getDireccion() == "E") {
-            return -segunda.getPosM().getLongitud().getGrados() - (segunda.getPosM().getLongitud().getGrados());
+            return -segunda.getPosM().getLongitud().getGrados() - (primera.getPosM().getLongitud().getGrados());
         }
         if (segunda.getPosM().getLongitud().getDireccion() == "E" && primera.getPosM().getLongitud().getDireccion() == "W") {
-            return segunda.getPosM().getLongitud().getGrados() - (-segunda.getPosM().getLongitud().getGrados());
+            return segunda.getPosM().getLongitud().getGrados() - (-primera.getPosM().getLongitud().getGrados());
         }
-        return segunda.getPosM().getLongitud().getGrados() - (segunda.getPosM().getLongitud().getGrados());
+        return segunda.getPosM().getLongitud().getGrados() - (primera.getPosM().getLongitud().getGrados());
     }
 
     public int alturaPixeles() {
@@ -30,44 +50,59 @@ public class Conversion {
 
     public int alturaGrados() {
         if (segunda.getPosM().getLatitud().getDireccion() == "S" && primera.getPosM().getLatitud().getDireccion() == "S") {
-            return -segunda.getPosM().getLatitud().getGrados() - (-segunda.getPosM().getLatitud().getGrados());
+            return -segunda.getPosM().getLatitud().getGrados() - (-primera.getPosM().getLatitud().getGrados());
         }
         if (segunda.getPosM().getLatitud().getDireccion() == "S" && primera.getPosM().getLatitud().getDireccion() == "N") {
-            return -segunda.getPosM().getLatitud().getGrados() - (segunda.getPosM().getLatitud().getGrados());
+            return -segunda.getPosM().getLatitud().getGrados() - (primera.getPosM().getLatitud().getGrados());
         }
         if (segunda.getPosM().getLatitud().getDireccion() == "S" && primera.getPosM().getLatitud().getDireccion() == "N") {
-            return segunda.getPosM().getLatitud().getGrados() - (-segunda.getPosM().getLatitud().getGrados());
+            return segunda.getPosM().getLatitud().getGrados() - (-primera.getPosM().getLatitud().getGrados());
         }
-        return segunda.getPosM().getLatitud().getGrados() - (segunda.getPosM().getLatitud().getGrados());
+        return segunda.getPosM().getLatitud().getGrados() - (primera.getPosM().getLatitud().getGrados());
     }
 
     // t = x - a / b - a
-    public double latitudT(Coordenada aux) {
-        double lat = aux.getPosM().getLatitud().latitudToDecimal();
+    public double latitudT(Latitud aux) {
+        double lat = aux.latitudToDecimal();
         if (segunda.getPosM().getLatitud().getDireccion() == "S" && primera.getPosM().getLatitud().getDireccion() == "S") {
-            return (lat - (primera.getPosM().getLatitud().getGrados()) / segunda.getPosM().getLatitud().getGrados() - (primera.getPosM().getLatitud().getGrados()));
+            return (lat - (primera.getPosM().getLatitud().getGrados()) / (segunda.getPosM().getLatitud().getGrados() - (primera.getPosM().getLatitud().getGrados())));
         }
         if (segunda.getPosM().getLatitud().getDireccion() == "S" && primera.getPosM().getLatitud().getDireccion() == "N") {
-            return (lat - (-primera.getPosM().getLatitud().getGrados()) / segunda.getPosM().getLatitud().getGrados() - (-primera.getPosM().getLatitud().getGrados()));
+            return (lat - (-primera.getPosM().getLatitud().getGrados()) / (segunda.getPosM().getLatitud().getGrados() - (-primera.getPosM().getLatitud().getGrados())));
         }
-        if (segunda.getPosM().getLatitud().getDireccion() == "N" && primera.getPosM().getLatitud().getDireccion() == "S") {
-            return (lat - (primera.getPosM().getLatitud().getGrados()) / -segunda.getPosM().getLatitud().getGrados() - (primera.getPosM().getLatitud().getGrados()));
+        if (segunda.getPosM().getLatitud().getDireccion() == "N" && primera.getPosM().getLatitud().getDireccion() == "N") {
+            return ((-lat - (-primera.getPosM().getLatitud().getGrados())) / (-segunda.getPosM().getLatitud().getGrados() - (-primera.getPosM().getLatitud().getGrados())));
+            
         }
-        return (lat - (-primera.getPosM().getLatitud().getGrados()) / -segunda.getPosM().getLatitud().getGrados() - (-primera.getPosM().getLatitud().getGrados()));
+        return (-lat - (primera.getPosM().getLatitud().getGrados()) / (-segunda.getPosM().getLatitud().getGrados() - (primera.getPosM().getLatitud().getGrados())));
     }
 
     // t = x - a / b - a
-    public double longitudT(Coordenada aux) {
-        double log = aux.getPosM().getLongitud().longitudToDecimal();
+    public double longitudT(Longitud aux) {
+        double log = aux.longitudToDecimal();
+        
         if (segunda.getPosM().getLongitud().getDireccion() == "E" && primera.getPosM().getLongitud().getDireccion() == "E") {
-            return (log - (primera.getPosM().getLongitud().getGrados()) / segunda.getPosM().getLongitud().getGrados() - (primera.getPosM().getLongitud().getGrados()));
+            return (log - (primera.getPosM().getLongitud().getGrados()) / (segunda.getPosM().getLongitud().getGrados() - (primera.getPosM().getLongitud().getGrados())));
         }
         if (segunda.getPosM().getLongitud().getDireccion() == "E" && primera.getPosM().getLatitud().getDireccion() == "W") {
-            return (log - (-primera.getPosM().getLongitud().getGrados()) / segunda.getPosM().getLongitud().getGrados() - (-primera.getPosM().getLongitud().getGrados()));
+            return (log - (-primera.getPosM().getLongitud().getGrados()) / (segunda.getPosM().getLongitud().getGrados() - (-primera.getPosM().getLongitud().getGrados())));
         }
         if (segunda.getPosM().getLongitud().getDireccion() == "W" && primera.getPosM().getLongitud().getDireccion() == "E") {
-            return (log - (primera.getPosM().getLongitud().getGrados()) / -segunda.getPosM().getLongitud().getGrados() - (primera.getPosM().getLongitud().getGrados()));
+            return (-log - (primera.getPosM().getLongitud().getGrados()) / (-segunda.getPosM().getLongitud().getGrados() - (primera.getPosM().getLongitud().getGrados())));
         }
-        return (log - (-primera.getPosM().getLongitud().getGrados()) / -segunda.getPosM().getLongitud().getGrados() - (-primera.getPosM().getLongitud().getGrados()));
+        
+        System.out.println(log);
+         System.out.println(primera.getPosM().getLongitud().getGrados());
+        System.out.println((-log - (-primera.getPosM().getLongitud().getGrados())));
+        System.out.println((-segunda.getPosM().getLongitud().getGrados() - (-primera.getPosM().getLongitud().getGrados())));
+        
+        return ((-log + (primera.getPosM().getLongitud().getGrados())) / (-segunda.getPosM().getLongitud().getGrados() - (-primera.getPosM().getLongitud().getGrados())));
+    }
+    
+    public double longitudPix(Longitud aux){
+        return primera.getPosI().getX() + longitudT(aux) * (segunda.getPosI().getX() - primera.getPosI().getX());
+    }
+    public double latitudPix(Latitud aux){
+        return primera.getPosI().getY() + latitudT(aux) * (segunda.getPosI().getY() - primera.getPosI().getY());
     }
 }
